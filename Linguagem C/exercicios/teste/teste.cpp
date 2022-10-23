a61e16fff1d61ccbfa996b3183cfe6fd26f61067
Eractrus de Judá <eractrus@gmail.com>
2022-10-23 14:56:29 -0300

    estudos css

---
+++ Linguagem C/exercicios/teste/teste.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int valor1 = 10;
+    int valor2 = 5;
+    int soma = 0;
+
+    printf("Digite o primeiro valor:\n");
+    scanf("%d", &valor1);
+
+    printf("digite o segundo valor:\n");
+    scanf("%d", &valor2);
+
+    soma = valor1 + valor2;
+
+    printf("Total :\n %d", soma)
+    
+
+    
+    return 0;
+}
