5dfd0af770e937034d8654822b224d5d49cc8868
Eractrus de Judá <eractrus@gmail.com>
2022-12-09 06:33:05 -0300

    estudos JS iniciado

---
+++ JavaScript/Variáveis/variaveis.js
@@ -0,0 +1,45 @@
+// Conceitos
+
+// Variáveis são memórias que armazenam dados em determinado tempo
+// Principais tipos de variáveis 
+
+
+
+/* String, Number, Boolean
+
+String - Armazena textos
+
+Number - Armazena números
+    int - npumeros inteiros
+    float - números flutuantes
+
+Bollean - True our false
+    True - verdadeiro
+    False - falso
+
+        A variável Boolean é utilizada con finalidade de atribuir uma condição 
+*/
+
+// Declaramos uma variável com a instrução "var"
+
+//---------------------------------------------------------------------------//
+
+// Variável string
+
+var texto = 'Eractrus'
+
+// Variável number
+
+var numeroInteiro = -7
+var numeroFracionado = 1024.35
+// Variável boolean
+
+var verificar = true
+
+//------------------------------------Comandos----------------------------------
+
+//alert(texto) // Serve para abrir um dialog 
+
+//document.write(texto)// podemos escrever textos no navegador
+
+//console.log(texto)
