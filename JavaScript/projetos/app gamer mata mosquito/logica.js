c25f294718beca810aaac335d73dbcf0f5106658
Eractrus de Judá <eractrus@gmail.com>
2023-02-08 20:45:09 -0300

    app mata mosquito

--- JavaScript/projetos/app gamer mata mosquito/logica.js
+++ JavaScript/projetos/app gamer mata mosquito/logica.js
@@ -1,29 +1,114 @@
 
 // verificação de tamanho de tela
 
-    var altura = 0
-    var largura = 0
+var altura = 0
+var largura = 0
+var vidas = 1
+var tempo = 60
 
-    function ajustedetela(){
-        altura = window.innerHeight
-        largura = window.innerWidth
+function ajustedetela(){
+    altura = window.innerHeight
+    largura = window.innerWidth
 
-        console.log('Altura = ' + altura, 'Largura = ' + largura )
-    }
+    console.log(altura, largura)
+}
 
     ajustedetela()
 
-// fim da verificação do tamanho da tela
+// cronometro
+var cronometro = setInterval(function(){
+
+    tempo -= 1
+
+        if(tempo < 0){
+            window.location.href="vitoria.html"
+        }else{
+            document.getElementById('cronometro').innerHTML = tempo
+        }
+    
+    
+}, 1000)
 
 //posição aleatória para os mosquitos
+function posicaoRandomica(){
 
-    var positionx = Math.floor(Math.random() * altura)
-    var positiony = Math.floor(Math.random() * largura)
+// remover mosquito caso exista 
+
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+
+    //contagem de vidas + redirecionamento para tela fim
+        if(vidas > 3){
+            window.location.href="fim_de_jogo.html"
+        }else{
+            document.getElementById('v' + vidas ).src="imagens/imagens/coracao_vazio.png"
+
+            vidas++
+        }
+    }
+
+    var positionx = Math.floor(Math.random() * largura) - 100
+    var positiony = Math.floor(Math.random() * altura) - 100
+
+    positionx = positionx < 0 ? 0 : positionx
+    positiony = positiony < 0 ? 0 : positiony
 
     console.log(positionx, positiony)
 
-//fim do calculo de posição aleatória
+    //fim do calculo de posição aleatória
 
-// introduzir o elemento html (mosquito)
+    // introduzir o elemento html (mosquito)
 
+    var mosquito = document.createElement('img')
+    mosquito.src='imagens/imagens/mosca.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = positionx + 'px'
+    mosquito.style.top = positiony + 'px'
+    mosquito.style.position = 'absolute' 
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
+    document.body.appendChild(mosquito)
+
+    tamanhoAleatorio()// retorna o tamnho aleatório do mosquito
+
+    ladoAleatorio()// retorna o lado do mosquito de forma aleatória
+}
+
+
+// tamanho aleatório do mosquito
+function tamanhoAleatorio(){
+
+    var classe = Math.floor(Math.random()*3)
+
+        switch(classe){
+            case 0:
+                return 'mosquito1'
+
+            case 1:
+                return 'mosquito2'
+                
+            case 2:
+                return 'mosquito3'   
+                
+        }
+}
+
+
+// posição do mosquito 
+function ladoAleatorio(){
+
+    var classe = Math.floor(Math.random()*2)
+
+    switch(classe){
+        case 0:
+            return 'ladoA'
+
+        case 1:
+            return 'ladoB'
+  
+            
+    }
+}
 
