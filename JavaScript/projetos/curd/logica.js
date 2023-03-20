0950852278d5e3c8dfd0e9c175ca63ba0c259bbe
Eractrus de Judá <eractrus@gmail.com>
2023-03-19 21:27:18 -0300

    estudos

--- JavaScript/projetos/curd/logica.js
+++ JavaScript/projetos/curd/logica.js
@@ -3,20 +3,20 @@
 const form = document.querySelector('form')
 const ul = document.querySelector('ul')
 
-
     form.onsubmit = function(event){
         event.preventDefault()
 
-            const input = form.querySelector('input')
-                const value = input.value
+      
+        const input = form.querySelector('input')
+        const value = input.value
 
-                        if(input.value == ""){
-                            alert('Digite uma tarefa válida')
-                        }else{
-                            input.value = ""
-                            ul.innerHTML += `<li class="lista ">${value} <button class="fechar delete">X</button></li>`
-                        }
-                 
+        if(input.value == ""){
+            alert('Digite uma tarefa válida')
+        }else{
+            input.value = ""
+            ul.innerHTML += `<li class="lista">${value} <button class="fechar delete">X</button></li>`
+        }
+
 
         ul.onclick = function(event){
             if(event.target.classList.contains('delete')){
---
+++ python/estruturas de dados/algoritimos_de_busca.py
@@ -0,0 +1,76 @@
+# os algoritmos resolvem problemas relacionados ao encontro de valores em uma estrutura de dados
+
+
+#lista = ['maria', 'joão', 'josé']
+#valor = input('Digite o nome do aluno: ')
+
+              
+#if(valor in lista):# realização de busca na lista de alunos acima
+   # print(f'está matriculado ')
+
+#else:
+   # print(f'aluno não matriculado ')
+   # print(f'Deseja realizar matricula? ')
+
+#realizarMarticula = input('sim ou não ? ')
+
+#if(realizarMarticula == 'sim'):
+
+   # CursoDesejado = input('Digite o curso desejado: ')
+   # nome = input('Digite seu nome: ')
+   # endereco = input('Digite seu endereço: ')
+   # idade = input('Digite sua idade: ')
+
+    #print('nome: ', nome)
+   # print('Curso: ', CursoDesejado)
+    #print('Idade: ', idade)
+   # print('Endereço: ', endereco)
+
+    #print(f'Pré-matricula realizada com sucesso, em instante enviaremos um e-mail de confirmação a UniErac agradesce seu contato. Nos vemos em breve')
+
+
+#else:
+    #print(f'Finalizamos por aqui, até a próxima: ')
+
+
+
+
+# list comprehension - compreesões de lista
+
+# lista é uma estrutura de dados do tipo sequencial qu possui  como principal característica se mutável . 
+# Novos valores podem ser adicionados ou removidos dessa lista 
+
+
+# criando uma lista
+
+lista1 = []
+
+lista2= ['a','b','c']
+
+# lista - list comprehension / dinãmica
+
+#[x for x in iterable]
+
+# lista usando o contrutoe de tipo 
+
+list()
+
+#print(lista1)
+#print(lista2)
+
+# funções .map() e .filter()
+
+# tuplas 
+
+lista = [1,15,88,22, 2, 159 ,102, 3, 34, 35, 46, 75, 243, 31, 335]
+
+lista.sort()
+
+print(lista)
+
+
+
+
+
+
+
